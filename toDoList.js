9aa3cd7ba5d12d4caf697ef8ea726df031ae96c4
ROKONUZZAMAN1 <mrokon7272@gmail.com>
2025-09-15 12:15:25 +0600

    to do list

---
+++ toDoList.js
@@ -0,0 +1,196 @@
+// input 
+const inputbox = document.getElementById('input-Box')
+const listContainer = document.getElementById('list-container')
+function addtask() {
+    if (inputbox.value === '') {
+        alert('You must write something')
+    }
+    else {
+        let li = document.createElement('li');
+        li.innerHTML = inputbox.value;
+        listContainer.appendChild(li)
+        let span = document.createElement('span')
+        span.innerHTML = '\u00d7'
+        li.appendChild(span)
+    }
+    inputbox.value = ""
+
+}
+document.getElementById('list-container').addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('extra')
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove()
+    }
+}, false);
+
+
+// local storage part
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
